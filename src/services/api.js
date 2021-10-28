81929d3c8bceede08f0245bb830b6c223e69d53d
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-28 17:21:55 +0530

    Get shows details for a particular movie

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,38 @@
-  export function getMovies() {
-	let url = "https://backend-database-movie-entity.herokuapp.com/api/movies";
+//Movies
+
+export function getMovies() {
+  let url = "https://backend-database-movie-entity.herokuapp.com/api/movies";
+  let meta = {
+    method: "GET",
+  };
+
+  return makeRequest(url, "", meta);
+}
+
+
+//shows
+
+export function getMovieShows(id) {
+	let url = `https://backend-database-movie-entity.herokuapp.com/api/shows/${id}`
+  
 	let meta = {
 	  method: "GET",
 	};
   
 	return makeRequest(url, "", meta);
   }
-  
+
 
 function makeRequest(url, queryParams, meta) {
-	url += `${queryParams}`;
-	return fetch(url, meta)
-	  .then((response) => {
-		return response.json();
-	  })
-	  .then((data) => {
-		return data;
-	  })
-	  .catch((err) => {
-		console.error(err);
-	  });
-  }
+  url += `${queryParams}`;
+  return fetch(url, meta)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
