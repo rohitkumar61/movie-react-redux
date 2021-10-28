e67b0210f3dc631f94b230f2a0e86b4b0878661c
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-28 16:34:27 +0530

    Add router to component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import './App.css';
+import Header from "./layout/Header";
+import MovieContainer from "./components/Movie/MovieContainer";
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     hello world
+      <Header />
+      <Switch>
+      <Route path="/" exact component={MovieContainer} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
