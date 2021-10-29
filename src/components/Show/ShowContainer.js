9da6a4283136f0d7507e60007d3764394c988cc9
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-29 10:20:20 +0530

    Add handle add show

--- src/components/Show/ShowContainer.js
+++ src/components/Show/ShowContainer.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 
 import * as ShowsApi from "./../../services/api.js";
+import MovieShows from "./MovieShows.js";
+import ShowsModal from "./ShowsModal";
 
 class ShowContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showData: [],
+      showData: { shows: [] },
     };
   }
 
   fetchMovieShows = async () => {
     let shows = await ShowsApi.getMovieShows(this.props.match.params.id);
+    console.log("--------------------data from api");
+    console.log(shows);
     this.setState({ showData: shows });
   };
 
@@ -19,12 +23,34 @@ class ShowContainer extends React.Component {
     this.fetchMovieShows();
   }
 
+  handleAddShow = async(data) => {
+    let id = parseInt(this.props.match.params.id);
+    let newShow = await ShowsApi.addNewShows(data,id);
+    
+    this.setState({
+      showData:{shows:[...this.state.showData.shows,newShow]}
+    });
+  };
+
   render() {
+    console.log("------------------------------data from state---------");
+    console.log(this.state.showData.shows);
     return (
-      <div
-        className="d-flex m-2"
-        style={{ overflowX: "auto", flex: "1" }}
-      ></div>
+      <div className="d-flex justify-content-center flex-column">
+        {this.state.showData.shows.map((show) => (
+          <MovieShows
+            show={show}
+            key={show.id}
+            onDelete={() => this.handleDeleteshow(show.id)}
+          />
+        ))}
+        <div>
+          <ShowsModal
+            key={this.state.showData.shows.id}
+            onCreate={this.handleAddShow}
+          />
+        </div>
+      </div>
     );
   }
 }
