9c682cecfb517eca7dede3991292e4b78333a7b0
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-29 07:27:22 +0530

    Update the component name from movie modal to update movie modal

--- src/components/Movie/MovieCard.js
+++ src/components/Movie/MovieCard.js
@@ -1,17 +1,17 @@
 // import Button from "@restart/ui/esm/Button";
 import React, { Component } from "react";
-import { Card} from "react-bootstrap";
-import {Button} from "react-bootstrap"
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-import UpdateMovie from "./UpdateMovie"
+import UpdateMovieModal from "./UpdateMovieModal";
 
 class MovieCard extends React.Component {
   render() {
-    const {id, title, director, year, length_minutes } = this.props.movie;
+    const { id, title, director, year, length_minutes } = this.props.movie;
 
     return (
-		<div>
+      <div>
         <Card
           style={{
             width: "26rem",
@@ -21,28 +21,38 @@ class MovieCard extends React.Component {
             backgroundColor: "blue",
           }}
         >
-          <Link to={`/movie/${id}`}> 
-    
-          <Card.Body style={{marginTop:"2.3rem",fontSize:"3rem",lineHeight:"4rem"}}>
-            <Card.Title><h1>{title}</h1></Card.Title>
-            <Card.Title><h2>{director}</h2></Card.Title>
-            <Card.Title><h2>{year}</h2></Card.Title>
-            <Card.Title><h2>{length_minutes}min</h2></Card.Title>
-          </Card.Body>
+          <Link to={`/movie/${id}`}>
+            <Card.Body
+              style={{
+                marginTop: "2.3rem",
+                fontSize: "3rem",
+                lineHeight: "4rem",
+              }}
+            >
+              <Card.Title>
+                <h1>{title}</h1>
+              </Card.Title>
+              <Card.Title>
+                <h2>{director}</h2>
+              </Card.Title>
+              <Card.Title>
+                <h2>{year}</h2>
+              </Card.Title>
+              <Card.Title>
+                <h2>{length_minutes}min</h2>
+              </Card.Title>
+            </Card.Body>
           </Link>
         </Card>
-       
-    
-	 
-     <Button variant="outline-danger" onClick={this.props.onDelete}  size="lg">Delete Movie</Button>{' '}
-    
-     <UpdateMovie
-     key = {id}
-     id = {id}
-     onUpdate = {this.props.onUpdate}
-     />
-    
-     </div>
+        <Button
+          variant="outline-danger"
+          onClick={this.props.onDelete}
+          size="lg"
+        >
+          Delete Movie
+        </Button>{" "}
+        <UpdateMovieModal key={id} id={id} onUpdate={this.props.onUpdate} />
+      </div>
     );
   }
 }
