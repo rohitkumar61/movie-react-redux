c15d16a7a1d91deb28f1ab34248633bf1084d18e
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-29 11:19:01 +0530

    Delete shows ajax calls

--- src/services/api.js
+++ src/services/api.js
@@ -62,6 +62,17 @@ export function deleteMovie(id) {
   return makeRequest(url, "", meta);
 }
 
+
+export function deleteShows(id) {
+  let url = `https://backend-database-movie-entity.herokuapp.com/api/shows/${id}`;
+  let meta = {
+    method: "DELETE",
+  };
+
+  return makeRequestDelete(url, "", meta);
+}
+
+
 export function updateMovie(data,id) {
   let url = `https://backend-database-movie-entity.herokuapp.com/api/movies/${id}`;
   let meta = {
@@ -75,6 +86,25 @@ export function updateMovie(data,id) {
   return makeRequest(url,"", meta);
 }
 
+
+
+function makeRequestDelete(url, queryParams, meta) {
+  url += `${queryParams}`;
+  return fetch(url, meta)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data)
+      return parseInt(data.id);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+
+
 function makeRequest(url, queryParams, meta) {
   url += `${queryParams}`;
   return fetch(url, meta)
@@ -82,6 +112,7 @@ function makeRequest(url, queryParams, meta) {
       return response.json();
     })
     .then((data) => {
+      console.log(data)
       return data;
     })
     .catch((err) => {
