3262c7a986a2105b19c5e9fd911272ccc9214212
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-29 10:24:25 +0530

    Add new show ajax calls

--- src/services/api.js
+++ src/services/api.js
@@ -9,19 +9,71 @@ export function getMovies() {
   return makeRequest(url, "", meta);
 }
 
-
 //shows
 
 export function getMovieShows(id) {
-	let url = `https://backend-database-movie-entity.herokuapp.com/api/shows/${id}`
-  
-	let meta = {
-	  method: "GET",
-	};
-  
-	return makeRequest(url, "", meta);
-  }
+  let url = `https://backend-database-movie-entity.herokuapp.com/api/movies/${id}/shows`;
 
+  let meta = {
+    method: "GET",
+  };
+
+  return makeRequest(url, "", meta);
+}
+
+
+export function addNewMovie(data) {
+  const url = "https://backend-database-movie-entity.herokuapp.com/api/movies";
+  const meta = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  return makeRequest(url,"", meta);
+  
+}
+
+
+
+export function addNewShows(data,id) {
+  const url = `https://backend-database-movie-entity.herokuapp.com/api/shows/${id}`;
+  const meta = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  return makeRequest(url,"", meta);
+  
+}
+
+
+export function deleteMovie(id) {
+  let url = `https://backend-database-movie-entity.herokuapp.com/api/movies/${id}`;
+  let meta = {
+    method: "DELETE",
+  };
+
+  return makeRequest(url, "", meta);
+}
+
+export function updateMovie(data,id) {
+  let url = `https://backend-database-movie-entity.herokuapp.com/api/movies/${id}`;
+  let meta = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  return makeRequest(url,"", meta);
+}
 
 function makeRequest(url, queryParams, meta) {
   url += `${queryParams}`;
