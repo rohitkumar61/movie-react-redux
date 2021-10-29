6c6d6df777cf98e08a772ad1f2865d4411a64d9c
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-29 07:19:51 +0530

    Change link and include update movie component

--- src/components/Movie/MovieCard.js
+++ src/components/Movie/MovieCard.js
@@ -1,29 +1,48 @@
+// import Button from "@restart/ui/esm/Button";
 import React, { Component } from "react";
-import { Card } from "react-bootstrap";
+import { Card} from "react-bootstrap";
+import {Button} from "react-bootstrap"
+import { Link } from "react-router-dom";
+
+import UpdateMovie from "./UpdateMovie"
 
 class MovieCard extends React.Component {
   render() {
-    const { title, director, year, length_minutes } = this.props.movie;
+    const {id, title, director, year, length_minutes } = this.props.movie;
 
     return (
-      <div>
+		<div>
         <Card
           style={{
-            width: "18rem",
-            height: "7rem",
+            width: "26rem",
+            height: "19rem",
             margin: "20px",
             color: "white",
             backgroundColor: "blue",
           }}
         >
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Title>{director}</Card.Title>
-            <Card.Title>{year}</Card.Title>
-            <Card.Title>{length_minutes}</Card.Title>
+          <Link to={`/movie/${id}`}> 
+    
+          <Card.Body style={{marginTop:"2.3rem",fontSize:"3rem",lineHeight:"4rem"}}>
+            <Card.Title><h1>{title}</h1></Card.Title>
+            <Card.Title><h2>{director}</h2></Card.Title>
+            <Card.Title><h2>{year}</h2></Card.Title>
+            <Card.Title><h2>{length_minutes}min</h2></Card.Title>
           </Card.Body>
+          </Link>
         </Card>
-      </div>
+       
+    
+	 
+     <Button variant="outline-danger" onClick={this.props.onDelete}  size="lg">Delete Movie</Button>{' '}
+    
+     <UpdateMovie
+     key = {id}
+     id = {id}
+     onUpdate = {this.props.onUpdate}
+     />
+    
+     </div>
     );
   }
 }
